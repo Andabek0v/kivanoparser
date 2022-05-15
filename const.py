59a8aea2d54bed80f3653f3da4618be6bd059458
Isabar Andabekov <andabekov2280@gmail.com>
2022-05-15 17:31:50 +0600

    Constant values

---
+++ const.py
@@ -0,0 +1,5 @@
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36', 
+    'accept': '*/*'
+}
+DOMEN = 'https://www.kivano.kg'
